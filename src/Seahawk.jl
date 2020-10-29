06258c491c21128a4fa8588ed90633c6f7cd7397
Ole Kröger <o.kroeger@opensourc.es>
2020-10-29 16:37:43 +0100

    using DocSeeker

--- src/Seahawk.jl
+++ src/Seahawk.jl
@@ -2,73 +2,82 @@ module Seahawk
 
 using Base.Docs: modules, DocStr, MultiDoc, parsedoc
 using CodeTracking
+using DocSeeker
 using REPL: doc, meta, stripmd
 using ReplMaker
-using Markdown
+using Markdown: MD
 
-struct SearchResult
-    doc::DocStr
-    text :: AbstractString
-    summary :: AbstractString
-    relevance :: Float64
-end
-
-function searchdoc!(search_results, haystack::MultiDoc, needle)
-    for v in values(haystack.docs)
-        searchdoc!(search_results, v, needle) && return true
-    end
-    false
-end
-
-function searchdoc!(search_results, haystack::DocStr, needle)
-    searchdoc!(search_results, haystack, parsedoc(haystack), needle) && return true
-    if haskey(haystack.data, :fields)
-        for doc in values(haystack.data[:fields])
-            searchdoc!(search_results, haystack, doc, needle) && return true
-        end
-    end
-    false
-end
-
-## Markdown search simply strips all markup and searches plain text version
-searchdoc!(search_results, doc, haystack::Markdown.MD, needle) = 
-    searchdoc!(search_results, doc, stripmd(haystack.content), needle)
-
-function searchdoc!(search_results, doc, haystack::AbstractString, needle)
-    pos = findfirst(needle, haystack)
-    pos === nothing && return false
-    push!(search_results, SearchResult(doc, haystack, haystack, 0.0))
-    return true
-end
+const DESCRIPTION_LIMIT = 80
 
 search(string) = search(stdout, string)
 function search(io::IO, needle)
-    search_results = Vector{SearchResult}()
-    for mod in modules
-        # Module doc might be in README.md instead of the META dict
-        # searchdoc!(search_results, doc(mod), needle)
-        for (k, v) in meta(mod)
-            searchdoc!(search_results, v, needle)
-        end
-    end
+    search_results = searchdocs(needle)
     print_results(search_results)
 end
 
+flatten(md::MD) = MD(flat_content(md))
+
+# Faster version
+
+function flat_content!(xs, out = [])
+  for x in xs
+    if isa(x, MD)
+      flat_content!(x.content, out)
+    else
+      push!(out, x)
+    end
+  end
+  return out
+end
+
+flat_content(md::MD) = flat_content!(md.content)
+
+function strlimit(str::AbstractString, limit::Integer = 30, ellipsis::AbstractString = "…")
+    will_append = length(str) > limit
+  
+    io = IOBuffer()
+    i = 1
+    for c in str
+      will_append && i > limit - length(ellipsis) && break
+      isvalid(c) || continue
+  
+      print(io, c)
+      i += 1
+    end
+    will_append && print(io, ellipsis)
+  
+    return String(take!(io))
+end
+
+description(docs) = ""
+description(docs::MD) = begin
+  md = flatten(docs).content
+  for part in md
+    if part isa Markdown.Paragraph
+      desc = Markdown.plain(part)
+      occursin("No documentation found.", desc) && return ""
+      return strip(strlimit(desc, DESCRIPTION_LIMIT))
+    end
+  end
+  return ""
+end
+
 function print_results(search_results)
-    for search_result in search_results
-        print_result(search_result)
+    for search_result in reverse(search_results)
+        print_result(search_result[2])
     end
 end
 
-function print_result(res::SearchResult)
-    data = res.doc.data
-    path = CodeTracking.maybe_fix_path(data[:path])
-    printstyled(data[:binding]; color=:green)
-    print(" with ")
-    printstyled(data[:typesig]; color=:green)
+function print_result(res::DocSeeker.DocObj)
+    path = CodeTracking.maybe_fix_path(res.path)
+    summary = description(res.html)
+    printstyled(res.name; color=:green)
     print(" in ")
-    printstyled("$path:$(data[:linenumber])"; color=:blue)
+    printstyled(res.mod; color=:green)
+    print(" at ")
+    printstyled("$path:$(res.line)"; color=:blue)
     println()
+    println(summary)
 end
 
 function __init__()
